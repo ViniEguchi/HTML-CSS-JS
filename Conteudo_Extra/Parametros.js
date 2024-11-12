800040357e924d85cd63f7ef8b6c0159d70cb7ee
ViniEguchi <vinicius.eguchi@sptech.school>
2024-11-12 16:22:38 -0300

    Inicio Lista 12

---
+++ Conteudo_Extra/Parametros.js
@@ -0,0 +1,18 @@
+
+function teste(a, b) {
+    if (a == b) {
+        return "Ambos são iguais";
+    }
+
+    if (a > b) {
+        return a;
+    }
+    
+    return b;
+}
+
+console.log(teste(5, 6));
+console.log(teste(6, 6));
+console.log(teste(10, 15));
+console.log(teste(0, -6));
+console.log(teste(-15, -7));
